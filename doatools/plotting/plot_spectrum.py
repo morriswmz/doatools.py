db1de57a0d45cb362688a31996d60f41afe1795d
morriswmz <morriswmz@gmail.com>
2018-09-16 22:39:25 -0500

    Added support for plotting multiple 1D spectra.

--- doatools/plotting/plot_spectrum.py
+++ doatools/plotting/plot_spectrum.py
@@ -1,25 +1,65 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def _plot_spectrum_1d(sp, grid, estimates, ground_truth, use_log_scale, discrete):
-    x = grid.axes[0]
-    # Normalize
-    max_y = sp.max()
-    y = sp / max_y if max_y > 0 else sp
-    if discrete:
-        if use_log_scale:
-            plt.yscale('log')
-        container_sp = plt.stem(x, y, 'C0-', markerfmt=' ', basefmt=' ')
+def _normalize_by_maximum(x):
+    max_x = x.max()
+    if max_x > 0:
+        return x / max_x
     else:
-        if use_log_scale:
-            container_sp = plt.semilogy(x, y, 'C0-')
+        return x
+
+def _preprocess_spectrum_input(sp, grid):
+    '''
+    Preprocesses the spectrum input and convert it into a list of tuples, where
+    the first element in the tuple is the numpy array and the second element is
+    the label. Also outputs the dimension of the spectrum.
+    '''
+    if isinstance(sp, np.ndarray):
+        sp_list = [(sp, '')]
+    elif isinstance(sp, list) or isinstance(sp, tuple):
+        sp_list = [(s, '') for s in sp]
+    elif isinstance(sp, dict):
+        sp_list = [(v, k) for k, v in sp.items()]
+    else:
+        raise ValueError('Unsupported spectrum data input.')
+    if len(sp_list) == 0:
+        raise ValueError('Expecting at least on spectrum.')
+    for t in sp_list:
+        if t[0].shape != grid.shape:
+            raise ValueError('The shape of the spectrum does not match the search grid.')
+    return sp_list, sp_list[0][0].ndim
+
+def _plot_spectrum_1d(sp_list, grid, estimates, ground_truth, use_log_scale, discrete):
+    x = grid.axes[0]
+    has_legend = False
+    # 'C3' and 'C2' are reserved for estimates and ground truth
+    color_estimates = 'C3'
+    color_truth = 'C2'
+    color_cycle = ['C0', 'C1', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9']
+    plot_containers = []
+    # Plot every spectrum
+    if discrete and use_log_scale:
+        plt.yscale('log')
+    for i, (spectrum, label) in enumerate(sp_list):
+        color = color_cycle[i % len(color_cycle)]
+        y = _normalize_by_maximum(spectrum)
+        if len(label) > 0:
+            has_legend = True
+        if discrete:
+            container_sp = plt.stem(x, y, '-', markerfmt=' ', basefmt=' ',
+                                    label=label)
+            plt.setp(container_sp, color=color)
+            plot_containers.append(container_sp)
         else:
-            container_sp = plt.plot(x, y, 'C0-')
+            if use_log_scale:
+                plot_containers.append(plt.semilogy(x, y, '-', label=label, color=color))
+            else:
+                plot_containers.append(plt.plot(x, y, '-', label=label, color=color))
     # Set up x-axis
     x_presets = {
-        'rad': ('\\theta/rad', -np.pi/2, np.pi/2),
-        'deg': ('\\theta/deg', -90, 90),
-        'sin': ('\\sin\\theta', -1, 1)
+        'rad': ('DOA/rad', -np.pi/2, np.pi/2),
+        'deg': ('DOA/deg', -90, 90),
+        'sin': ('DOA/sin', -1, 1)
     }
     x_label, x_min, x_max = x_presets[grid.unit]
     plt.xlim((x_min, x_max))
@@ -34,22 +74,26 @@ def _plot_spectrum_1d(sp, grid, estimates, ground_truth, use_log_scale, discrete
             raise ValueError('The unit of estimates does not match that of the search grid.')
         x_est = estimates.locations
         y_est = np.ones(x_est.shape)
-        container_est = plt.stem(x_est, y_est, 'C3--', markerfmt='C3x',
+        container_est = plt.stem(x_est, y_est, '--', markerfmt='x',
                                  basefmt=' ', label='Estimates')
-    else:
-        container_est = None
+        plt.setp(container_est, color=color_estimates)
+        plot_containers.append(container_est)
+        has_legend = True
     # Plot ground truth
     if ground_truth is not None:
         if ground_truth.unit != grid.unit:
             raise ValueError('The unit of ground truth does not match that of the search grid.')
         x_truth = ground_truth.locations
         y_truth = np.ones(x_truth.shape)
-        container_truth = plt.stem(x_truth, y_truth, 'C1--', markerfmt='C1o',
+        container_truth = plt.stem(x_truth, y_truth, '--', markerfmt='o',
                                    basefmt=' ', label='Ground truth')
-    else:
-        container_truth = None
-    plt.legend()
-    return container_sp, container_est, container_truth
+        plt.setp(container_truth, color=color_truth)
+        plot_containers.append(container_truth)
+        has_legend = True
+    # Show the legend.
+    if has_legend:
+        plt.legend()
+    return plot_containers
 
 def plot_spectrum(sp, grid, estimates=None, ground_truth=None,
                   use_log_scale=False, discrete=False):
@@ -57,8 +101,14 @@ def plot_spectrum(sp, grid, estimates=None, ground_truth=None,
     Provides a convenient way to plot the given spectrum.
 
     Args:
-        sp: A numpy array representing the spectrum. Usually this is the output
-            of a spectrum-based estimator.
+        sp: Can be one of the following:
+            1. A numpy array representing the spectrum. Usually this is the
+               output of a spectrum-based estimator.
+            2. A list or tuple of numpy arrays of the same shape. This will
+               draw multiple spectra in the same plot.
+            3. A dictionary of numpy arrays of the same shape, where the keys
+               represent the labels. This will draw multiple spectra in the same
+               plot with labels.
         grid: The search grid used to generate the spectrum.
         estimates: A SourcePlacement instance containing the estimated source
             locations. Will be plotted if supplied. Default value is None.
@@ -70,11 +120,11 @@ def plot_spectrum(sp, grid, estimates=None, ground_truth=None,
             Default value is False.
     
     Returns:
-        *containers: A tuple of plot containers.
+        *containers: A list of plot containers.
     '''
-    if sp.shape != grid.shape:
-        raise ValueError('The shape of the spectrum does not match the search grid.')
-    if sp.ndim == 1:
-        return _plot_spectrum_1d(sp, grid, estimates, ground_truth, use_log_scale, discrete)
+    sp_list, sp_dim = _preprocess_spectrum_input(sp, grid)
+    if sp_dim == 1:
+        return _plot_spectrum_1d(sp_list, grid, estimates, ground_truth,
+                                 use_log_scale, discrete)
     else:
         raise NotImplementedError()
