Fixed the derivative computation for 1D farfield sources.

doatools/model/sources.py
@@ -91,13 +91,15 @@ class SourcePlacement(ABC):
 class FarField1DSourcePlacement(SourcePlacement):
 
     def __init__(self, locations, unit='rad'):
-        '''
+        r'''
         Creates a far-field 1D source placement.
 
         Args:
             locations: A list or 1D numpy array representing the source
                 locations.
-            unit: Can be 'rad', 'deg' or 'sin'.
+            unit: Can be 'rad', 'deg' or 'sin'. 'sin' is a special unit where
+                sine of the arrival angle is used instead of the arrival angle
+                itself. 
         '''
         if isinstance(locations, list):
             locations = np.array(locations)
@@ -127,16 +129,20 @@ class FarField1DSourcePlacement(SourcePlacement):
         if sensor_locations.shape[1] < 1 or sensor_locations.shape[1] > 3:
             raise ValueError('Sensor locations can only consists of 1D, 2D or 3D coordinates.')
         
+        if self._unit == 'sin':
+            return self._phase_delay_matrix_sin(sensor_locations, wavelength, derivatives)
+        else:
+            return self._phase_delay_matrix_rad(sensor_locations, wavelength, derivatives)
+        
+    def _phase_delay_matrix_rad(self, sensor_locations, wavelength, derivatives=False):
+        # Unit can only be 'rad' or 'deg'.
         # Unify to radians.
         if self._unit == 'deg':
             locations = np.deg2rad(self._locations)
-        elif self._unit == 'sin':
-            locations = np.arcsin(self._locations)
         else:
             locations = self._locations
         # Locations is an 1D vector, convert it to a one-row matrix.
         locations = locations[np.newaxis]
-
         s = 2 * np.pi / wavelength
         if sensor_locations.shape[1] == 1:
             D = s * sensor_locations * np.sin(locations)
@@ -150,11 +156,33 @@ class FarField1DSourcePlacement(SourcePlacement):
             if derivatives:
                 DD = s * (sensor_locations[:, 0] * np.cos(locations) -
                             sensor_locations[:, 1] * np.sin(locations))
-        
-        if derivatives:
-            return D, DD
+        return (D, DD) if derivatives else D
+
+    def _phase_delay_matrix_sin(self, sensor_locations, wavelength, derivatives=False):
+        # Locations is an 1D vector, convert it to a one-row matrix.
+        sin_vals = self._locations[np.newaxis]
+        s = 2 * np.pi / wavelength
+        if sensor_locations.shape[1] == 1:
+            D = s * sensor_locations * sin_vals
+            if derivatives:
+                # Note that if x = \sin\theta then
+                # \frac{\partial cx}{\partial x} = cx
+                # This is difference from the derivative w.r.t. \theta:
+                # \frac{\partial cx}{\partial \theta} = c\cos\theta
+                DD = np.tile(s * sensor_locations, (1, self._locations.size))
         else:
-            return D
+            # The sources are assumed to be within the xy-plane. The offset
+            # along the z-axis of the sensors does not affect the delays.
+            cos_vals = np.sqrt(1.0 - sin_vals * sin_vals)
+            D = s * (sensor_locations[:, 0] * sin_vals +
+                        sensor_locations[:, 1] * cos_vals)
+            if derivatives:
+                # If x = \sin\theta, \theta \in (-\pi/2, \pi/2)
+                # a \sin\theta + b \cos\theta = ax + b\sqrt{1-x^2}
+                DD = s * (sensor_locations[:, 0] -
+                            sensor_locations[:, 1] * sin_vals / cos_vals)
+        return (D, DD) if derivatives else D
+
 
 class FarField2DSourcePlacement(SourcePlacement):
 

doatools/tests/test_source_placement.py
@@ -55,19 +55,40 @@ class TestSourcePlacement(unittest.TestCase):
         npt.assert_array_equal(sources_subset.locations, locations[:5])
 
     def test_far_field_1d_delay(self):
-        sources = FarField1DSourcePlacement(np.linspace(-np.pi/3, np.pi/4, 5))
         # 1D array
         sensor_locations_1 = np.array([0, 1, 2]).reshape((-1, 1))
-        D1, DD1 = sources.phase_delay_matrix(sensor_locations_1, self.wavelength, True)
+        # Unit: 'rad' or 'deg'
+        # Same locations in different units
+        sources_rad = FarField1DSourcePlacement(np.linspace(-np.pi/3, np.pi/4, 5))
+        sources_deg = FarField1DSourcePlacement(np.linspace(-60, 45, 5), 'deg')
+        for sources in [sources_rad, sources_deg]: 
+            # 1D array
+            D1, DD1 = sources.phase_delay_matrix(sensor_locations_1, self.wavelength, True)
+            D1_expected = np.array([
+                [  0.000000,  0.000000,  0.000000, 0.000000, 0.000000],
+                [ -5.441398, -3.490751, -0.820120, 2.019664, 4.442883],
+                [-10.882796, -6.981501, -1.640241, 4.039327, 8.885766]
+            ])
+            DD1_expected = np.array([
+                [0.000000,  0.000000,  0.000000,  0.000000, 0.000000],
+                [3.141593,  5.224278,  6.229432,  5.949737, 4.442883],
+                [6.283185, 10.448555, 12.458864, 11.899475, 8.885766]
+            ])
+            npt.assert_array_almost_equal(D1, D1_expected)
+            npt.assert_array_almost_equal(DD1, DD1_expected)
+        # Unit: 'sin'
+        sources_sin = FarField1DSourcePlacement(np.linspace(-0.4, 0.4, 5), 'sin')
+        # 1D array
+        D1, DD1 = sources_sin.phase_delay_matrix(sensor_locations_1, self.wavelength, True)
         D1_expected = np.array([
-            [  0.000000,  0.000000,  0.000000, 0.000000, 0.000000],
-            [ -5.441398, -3.490751, -0.820120, 2.019664, 4.442883],
-            [-10.882796, -6.981501, -1.640241, 4.039327, 8.885766]
+            [ 0.000000,  0.000000, 0.000000, 0.000000, 0.000000],
+            [-2.513274, -1.256637, 0.000000, 1.256637, 2.513274],
+            [-5.026548, -2.513274, 0.000000, 2.513274, 5.026548]
         ])
         DD1_expected = np.array([
-            [0.000000,  0.000000,  0.000000,  0.000000, 0.000000],
-            [3.141593,  5.224278,  6.229432,  5.949737, 4.442883],
-            [6.283185, 10.448555, 12.458864, 11.899475, 8.885766]
+            [ 0.000000,  0.000000,  0.000000,  0.000000,  0.000000],
+            [ 6.283185,  6.283185,  6.283185,  6.283185,  6.283185],
+            [12.566371, 12.566371, 12.566371, 12.566371, 12.566371]
         ])
         npt.assert_array_almost_equal(D1, D1_expected)
         npt.assert_array_almost_equal(DD1, DD1_expected)