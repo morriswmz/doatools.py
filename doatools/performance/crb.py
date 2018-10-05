2d416aad8f42f9e34b631a31b940a3f969f9c199
morriswmz <morriswmz@gmail.com>
2018-10-05 11:42:46 -0500

    Implemented remaining 1D CRB functions.

--- doatools/performance/crb.py
+++ doatools/performance/crb.py
@@ -1,6 +1,7 @@
 import numpy as np
+from ..model.sources import FarField1DSourcePlacement
 
-def _unify_source_power_input(p, k):
+def _unify_p_to_matrix(p, k):
     '''
     Unifies the source power input into a matrix.
     
@@ -12,10 +13,8 @@ def _unify_source_power_input(p, k):
         P = np.zeros((k, k))
         np.fill_diagonal(P, p)
         return P
-    if isinstance(p, list):
-        p = np.array(p)
-    if p.ndim == 1:
-        if p.size != k:
+    if isinstance(p, list) or p.ndim == 1:
+        if len(p) != k:
             raise ValueError('The length of the input vector does not match the number of sources.')
         return np.diag(p)
     elif p.ndim == 2:
@@ -23,15 +22,43 @@ def _unify_source_power_input(p, k):
     else:
         raise ValueError('Expecting a scalar, an 1D vector or a 2D matrix.')
 
+def _unify_p_to_vector(p, k):
+    '''
+    Unifies the source power input into a vector.
+    
+    Args:
+        p: A scalar, list, or a numpy array.
+        k: Number of sources.
+    '''
+    if np.isscalar(p):
+        return np.full((k,), p)
+    if isinstance(p, list) or p.ndim == 1:
+        if len(p) != k:
+            raise ValueError('The length of the input vector does not match the number of sources.')
+        return np.array(p)
+    elif p.ndim == 2:
+        return np.diag(p)
+    else:
+        raise ValueError('Expecting a scalar, an 1D vector or a 2D matrix.')
 
-def crb_sto_farfield_1d(design, wavelength, sources, p, sigma, n_snapshots=1):
+def crb_sto_farfield_1d(array, wavelength, sources, p, sigma, n_snapshots=1):
     r'''
-    Computes the stochastic CRB for 1D farfield sources. Under the stochastic
-    signal model, the source signal is assumed to be complex Gaussian, and the
-    noise signal is assumed complex white Gaussian.
+    Computes the stochastic CRB for 1D farfield sources.
+    
+    Under the stochastic signal model, the source signal is assumed to be
+    complex Gaussian, and the noise signal is assumed complex white Gaussian.
+    The unknown parameters include:
+
+    * Source locations.
+    * Real and imaginary parts of the source covariance matrix.
+    * Noise variance.
+    
+    This function only computes the CRB for source locations.
+    Because the unknown parameters do not include array perturbation parameters,
+    all array perturbation parameters are assumed known during the computation.
 
     Args:
-        design: Array design.
+        array: Array design.
         wavelength: Wavelength of the carrier wave.
         sources: A FarField1DSourcePlacement instance representing the source
             locations.
@@ -53,11 +80,13 @@ def crb_sto_farfield_1d(design, wavelength, sources, p, sigma, n_snapshots=1):
         Acoustics, Speech and Signal Processing, vol. 38, no. 10,
         pp. 1783-1795, Oct. 1990.
     '''
+    if not isinstance(sources, FarField1DSourcePlacement):
+        raise ValueError('Sources must be far-field and 1D.')
     k = sources.size
-    P = _unify_source_power_input(p, k)
-    A, D = design.steering_matrix(sources, wavelength, True)
+    P = _unify_p_to_matrix(p, k)
+    A, D = array.steering_matrix(sources, wavelength, True)
     A_H = A.T.conj()
-    I = np.eye(design.size)
+    I = np.eye(array.size)
     # Compute the covairance matrix: R = A P A^H + \sigma I
     R = A @ P @ A_H + sigma * I
     # Compute the projection matrix: A (A^H A)^{-1} A^H
@@ -69,9 +98,133 @@ def crb_sto_farfield_1d(design, wavelength, sources, p, sigma, n_snapshots=1):
     CRB = np.linalg.inv(CRB) * (sigma / n_snapshots / 2)
     return 0.5 * (CRB + CRB.T)
 
-def crb_det_farfield_1d(design, wavelength, sources, p, sigma, n_snapshots=1):
-    pass
+def crb_det_farfield_1d(array, wavelength, sources, P, sigma, n_snapshots=1):
+    r'''
+    Computes the deterministic CRB for 1D farfield sources.
+    
+    Under the deterministic signal model, the source signal is assumed to be
+    deterministic unknown, and the noise signal is assumed complex white
+    Gaussian. The unknown parameters include:
 
-def crb_stouc_farfield_1d(design, wavelength, sources, p, sigma, n_snapshots=1,
+    * Source locations.
+    * Real and imaginary parts of the source signals.
+    * Noise variance.
+
+    This function only computes the CRB for source locations.
+    Because the unknown parameters do not include array perturbation parameters,
+    all array perturbation parameters are assumed known during the computation.
+
+    Args:
+        array: Array design.
+        wavelength: Wavelength of the carrier wave.
+        sources: A FarField1DSourcePlacement instance representing the source
+            locations.
+        P: The sample covariance matrix of the source signals. Suppose there are
+            N snapshots, x(1), x(2), ..., x(N). The sample covariance matrix of
+            the source signals is obtained as follows:
+                \frac{1}{N} \sum_{t=1}^N x(t)x^H(t)
+        sigma: Variance of the additive noise.
+        n_snapshots: Number of snapshots.
+
+    Returns:
+        crb: k x k CRB matrix where k is the number of sources.
+    
+    References:
+    [1] P. Stoica and A. Nehorai, "Performance study of conditional and
+        unconditional direction-of-arrival estimation," IEEE Transactions on
+        Acoustics, Speech and Signal Processing, vol. 38, no. 10,
+        pp. 1783-1795, Oct. 1990.
+    '''
+    if not isinstance(sources, FarField1DSourcePlacement):
+        raise ValueError('Sources must be far-field and 1D.')
+    k = sources.size
+    m = array.size
+    if P.ndim != 2 or P.shape[0] != k or P.shape[1] != k:
+        raise ValueError('The sample covariance matrix of the source signals must be a K x K matrix, where K is the number of sources.')
+    A, D = array.steering_matrix(sources, wavelength, True)
+    # Compute the projection matrix: A (A^H A)^{-1} A^H
+    A_H = A.conj().T
+    P_A = A @ np.linalg.solve(A_H @ A, A_H)
+    # Compute the H matrix.
+    H = D.conj().T @ (np.eye(m) - P_A) @ D
+    # Compute the CRB.
+    CRB = np.linalg.inv((H * P.T).real) * (sigma / n_snapshots / 2)
+    return 0.5 * (CRB + CRB.T)
+
+def crb_stouc_farfield_1d(array, wavelength, sources, p, sigma, n_snapshots=1,
                           output_fim=False):
-    pass
+    r'''
+    Computes the stochastic CRB for 1D farfield sources with the assumption
+    that the sources are uncorrelated.
+    
+    Under the stochastic signal model, the source signal is assumed to be
+    uncorrelated complex Gaussian, and the noise signal is assumed complex
+    white Gaussian. The unknown parameters include:
+
+    * Source locations.
+    * Diagonals of the source covariance matrix.
+    * Noise variance.
+    
+    This function only computes the CRB for source locations.
+    Because the unknown parameters do not include array perturbation parameters,
+    all array perturbation parameters are assumed known during the computation.
+
+    Args:
+        array: Array design.
+        wavelength: Wavelength of the carrier wave.
+        sources: A FarField1DSourcePlacement instance representing the source
+            locations.
+        p: The power of the source signals. Can be
+            1. A scalar if all sources are uncorrelated and share the same
+               power.
+            2. A 1D numpy array if all sources are uncorrelated but have
+               different powers.
+            3. A 2D numpy array representing the source covariance matrix.
+               Only the diagonal elements will be used.
+        sigma: Variance of the additive noise.
+        n_snapshots: Number of snapshots.
+
+    Returns:
+        crb: k x k CRB matrix where k is the number of sources.
+    
+    References:
+    [1] H. L. Van Trees, Optimum array processing. New York: Wiley, 2002.
+    [2] M. Wang and A. Nehorai, "Coarrays, MUSIC, and the Cramér-Rao Bound,"
+        IEEE Transactions on Signal Processing, vol. 65, no. 4, pp. 933-946,
+        Feb. 2017.
+    [3] C-L. Liu and P. P. Vaidyanathan, "Cramér-Rao bounds for coprime and
+        other sparse arrays, which find more sources than sensors," Digital
+        Signal Processing, vol. 61, pp. 43-61, 2017.
+    '''
+    if not isinstance(sources, FarField1DSourcePlacement):
+        raise ValueError('Sources must be far-field and 1D.')
+    k = sources.size
+    p = _unify_p_to_vector(p, k)
+    m = array.size
+    # We need to compute each submatrix of the FIM.
+    A, DA = array.steering_matrix(sources, wavelength, True)
+    A_H = A.conj().T
+    DA_H = DA.conj().T
+    R = (A * p) @ A_H + sigma * np.eye(m)
+    R_inv = np.linalg.inv(R)
+    R_inv = 0.5 * (R_inv + R_inv.conj().T)
+    DRD = DA_H @ R_inv @ DA
+    DRA = DA_H @ R_inv @ A
+    ARD = A_H @ R_inv @ DA
+    ARA = A_H @ R_inv @ A
+    PP = np.outer(p, p)
+    FIM_tt = 2.0 * ((DRD.T * ARA + DRA.conj() * ARD) * PP).real
+    FIM_pp = (ARA.conj().T * ARA).real
+    R_inv2 = R_inv @ R_inv
+    FIM_ss = np.trace(R_inv2).real
+    # diag(A @ B) = np.sum(A^T * B, axis=0, keepdims=True)
+    FIM_tp = 2.0 * (DRA.conj() * (p[:, np.newaxis] * ARA)).real
+    FIM_ts = 2.0 * (p * np.sum(DA.conj() * (R_inv2 @ A), axis=0)).real[:, np.newaxis]
+    FIM_ps = np.sum(A.conj() * (R_inv2 @ A), axis=0).real[:, np.newaxis]
+    FIM = np.block([
+        [FIM_tt,          FIM_tp,          FIM_ts],
+        [FIM_tp.conj().T, FIM_pp,          FIM_ps],
+        [FIM_ts.conj().T, FIM_ps.conj().T, FIM_ss]
+    ])
+    CRB = np.linalg.inv(FIM)[:k, :k] / n_snapshots
+    return 0.5 * (CRB + CRB.T)
---
+++ doatools/tests/test_crb.py
@@ -0,0 +1,77 @@
+import unittest
+import numpy.testing as npt
+import numpy as np
+
+from doatools.model import UniformLinearArray, FarField1DSourcePlacement
+from doatools.performance import crb_det_farfield_1d, crb_sto_farfield_1d, crb_stouc_farfield_1d
+
+class TestCRB(unittest.TestCase):
+
+    def setUp(self):
+        self.wavelength = 1.0
+
+    def test_det_farfield_1d(self):
+        ula = UniformLinearArray(10, self.wavelength / 2)
+        sources = FarField1DSourcePlacement(np.linspace(-np.pi/3, np.pi/4, 3))
+        P = np.array([
+            [10.0    , 1.0+0.3j, 0.5-0.1j],
+            [1.0-0.3j,     11.0, 0.9-0.2j],
+            [0.5+0.1j, 0.9+0.2j,      9.0]
+        ])
+        sigma = 1.0
+        n_snapshots = 100
+        CRB_actual = crb_det_farfield_1d(ula, self.wavelength, sources, P, sigma, n_snapshots)
+        CRB_expected = np.array([
+            [ 2.636339e-06, -2.947961e-08, -2.894480e-08],
+            [-2.947961e-08,  5.868361e-07, -1.254243e-08],
+            [-2.894480e-08, -1.254243e-08,  1.495266e-06]
+        ])
+        npt.assert_allclose(CRB_actual, CRB_expected, rtol=1e-6)
+
+    def test_sto_farfield_1d(self):
+        ula = UniformLinearArray(10, self.wavelength / 2)
+        sources = FarField1DSourcePlacement(np.linspace(-np.pi/3, np.pi/4, 3))
+        P = np.array([
+            [10.0    , 1.0+0.3j, 0.5-0.1j],
+            [1.0-0.3j,     11.0, 0.9-0.2j],
+            [0.5+0.1j, 0.9+0.2j,      9.0]
+        ])
+        sigma = 1.0
+        n_snapshots = 100
+        CRB_actual = crb_sto_farfield_1d(ula, self.wavelength, sources, P, sigma, n_snapshots)
+        CRB_expected = np.array([
+            [ 2.663109e-06, -3.037374e-08, -2.994223e-08],
+            [-3.037374e-08,  5.922466e-07, -1.287051e-08],
+            [-2.994223e-08, -1.287051e-08,  1.512018e-06]
+        ])
+        npt.assert_allclose(CRB_actual, CRB_expected, rtol=1e-6)
+
+    def test_stouc_farfield_1d(self):
+        ula = UniformLinearArray(10, self.wavelength / 2)
+        sources = FarField1DSourcePlacement(np.linspace(-np.pi/3, np.pi/4, 3))
+        p = np.array([2.0, 3.0, 1.0])
+        sigma = 1.0
+        n_snapshots = 100
+        CRB_actual = crb_stouc_farfield_1d(ula, self.wavelength, sources, p, sigma, n_snapshots)
+        CRB_expected = np.array([
+            [ 1.3757938e-05, -3.7302575e-09, 4.3845873e-08],
+            [-3.7302575e-09,  2.2173076e-06, 5.0642214e-09],
+            [ 4.3845873e-08,  5.0642214e-09, 1.4740719e-05]
+        ])
+        npt.assert_allclose(CRB_actual, CRB_expected, rtol=1e-6)
+
+    def test_convergence_farfield_1d(self):
+        # The three CRBs should converge when SNR is sufficiently high.
+        ula = UniformLinearArray(16, self.wavelength / 2)
+        sources = FarField1DSourcePlacement(np.linspace(-np.pi/3, np.pi/4, 5))
+        p = np.diag(np.full((sources.size,), 1000.0))
+        sigma = 1.0 # 30 dB SNR
+        n_snapshots = 10
+        CRB_stouc = crb_stouc_farfield_1d(ula, self.wavelength, sources, p, sigma, n_snapshots)
+        CRB_sto = crb_sto_farfield_1d(ula, self.wavelength, sources, p, sigma, n_snapshots)
+        CRB_det = crb_det_farfield_1d(ula, self.wavelength, sources, p, sigma, n_snapshots)
+        npt.assert_allclose(np.diag(CRB_sto), np.diag(CRB_stouc), rtol=1e-2)
+        npt.assert_allclose(np.diag(CRB_det), np.diag(CRB_stouc), rtol=1e-2)
+
+if __name__ == '__main__':
+    unittest.main()
