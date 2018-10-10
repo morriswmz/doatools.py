2f23b5cbff3255ce741509279ee06e2939ac2bb9
morriswmz <morriswmz@gmail.com>
2018-10-10 15:31:57 -0500

    Added MRLA.

--- doatools/model/arrays.py
+++ doatools/model/arrays.py
@@ -436,6 +436,52 @@ class CoPrimeArray(GridBasedArrayDesign):
         '''Retrieves the mode used when creating this co-prime array.'''
         return self._mode
 
+_MRLA_PRESETS = [
+    [0],
+    [0, 1],
+    [0, 1, 3],
+    [0, 1, 4, 6],
+    [0, 1, 4, 7, 9],
+    [0, 1, 6, 9, 11, 13],
+    [0, 1, 8, 11, 13, 15, 17],
+    [0, 1, 4, 10, 16, 18, 21, 23],
+    [0, 1, 4, 10, 16, 22, 24, 27, 29],
+    [0, 1, 4, 10, 16, 22, 28, 30, 33, 35],
+    [0, 1, 6, 14, 22, 30, 32, 34, 37, 39, 41],
+    [0, 1, 6, 14, 22, 30, 38, 40, 42, 45, 47, 49],
+    [0, 1, 6, 14, 22, 30, 38, 46, 48, 50, 53, 55, 57],
+    [0, 1, 6, 14, 22, 30, 38, 46, 54, 56, 58, 61, 63, 65],
+    [0, 1, 6, 14, 22, 30, 38, 46, 54, 62, 64, 66, 69, 71, 73],
+    [0, 1, 8, 18, 28, 38, 48, 58, 68, 70, 72, 74, 77, 79, 81, 83],
+    [0, 1, 8, 18, 28, 38, 48, 58, 68, 78, 80, 82, 84, 87, 89, 91, 93],
+    [0, 1, 8, 18, 28, 38, 48, 58, 68, 78, 88, 90, 92, 94, 97, 99, 101, 103],
+    [0, 1, 8, 18, 28, 38, 48, 58, 68, 78, 88, 98, 100, 102, 104, 107, 109, 111, 113],
+    [0, 1, 10, 22, 34, 46, 58, 70, 82, 94, 106, 108, 110, 112, 114, 117, 119, 121, 123, 125]
+]
+
+class MinimumRedundancyLinearArray(GridBasedArrayDesign):
+
+    def __init__(self, n, d0, name=None, **kwargs):
+        '''Creates an n-element minimum redundancy linear array (MRLA).
+
+        Args:
+            n (int): Number of elements. Up to 20.
+            d0 (float): Fundamental inter-element spacing (usually smallest).
+            name (str): Name of the array design.
+            **kwargs: Other keyword arguments supported by ArrayDesign.
+        
+        References:
+        [1] M. Ishiguro, "Minimum redundancy linear arrays for a large number
+            of antennas," Radio Sci., vol. 15, no. 6, pp. 1163-1170, Nov. 1980.
+        [2] A. Moffet, "Minimum-redundancy linear arrays," IEEE Transactions
+            on Antennas and Propagation, vol. 16, no. 2, pp. 172-175, Mar. 1968.
+        '''
+        if n < 1 or n >= len(_MRLA_PRESETS):
+            raise ValueError('The MRLA presets only support up to 20 elements.')
+        if name is None:
+            name = 'MRLA {0}'.format(n)
+        super().__init__(np.array(_MRLA_PRESETS[n])[:, np.newaxis], d0, name, **kwargs)
+        
 class UniformCircularArray(ArrayDesign):
 
     def __init__(self, n, r, name=None, **kwargs):
