Added group sparsity based estimator and l1-svd preprocessing function. `_get_atom_matrix` now only handles caching and calls `_computation_atom_matrix` when the computation is required. Fixed a reference in the sparse covariance matching method.

doatools/estimation/sparse.py
@@ -2,7 +2,8 @@ import warnings
 from abc import ABC, abstractmethod
 import numpy as np
 from .core import SpectrumBasedEstimatorBase, ensure_covariance_size
-from ..optim.l1lsq import L1RegularizedLeastSquaresProblem
+from ..optim.l1lsq import L1RegularizedLeastSquaresProblem, \
+                          L21RegularizedLeastSquaresProblem
 from ..utils.math import khatri_rao, vec
 
 class SparseCovarianceMatching(SpectrumBasedEstimatorBase):
@@ -36,10 +37,9 @@ class SparseCovarianceMatching(SpectrumBasedEstimatorBase):
                 'constrainedl2'
 
         References:
-        [1] D. Malioutov, M. Cetin, and A. S. Willsky, "A sparse signal
-            reconstruction perspective for source localization with sensor
-            arrays," IEEE Transactions on Signal Processing, vol. 53, no. 8,
-            pp. 3010-3022, Aug. 2005.
+        [1] J. Yin and T. Chen, "Direction-of-Arrival Estimation Using a Sparse
+            Representation of Array Covariance Vectors," IEEE Transactions on
+            Signal Processing, vol. 59, no. 9, pp. 4489–4493, Sep. 2011.
         [2] Y. D. Zhang, M. G. Amin, and B. Himed, "Sparsity-based DOA
             estimation using co-prime arrays," in 2013 IEEE International
             Conference on Acoustics, Speech and Signal Processing (ICASSP),
@@ -60,28 +60,18 @@ class SparseCovarianceMatching(SpectrumBasedEstimatorBase):
         # Initialize the problem.
         self._problem = L1RegularizedLeastSquaresProblem(m, k, formulation, True)
 
-    def _get_atom_matrix(self, alt_grid=None):
-        if alt_grid is not None:
-            sources = alt_grid.source_placement
-            need_compute = True
-        else:
-            sources = self._search_grid.source_placement
-            need_compute = self._atom_matrix is None
-        if need_compute:
-            A = self._array.steering_matrix(sources, self._wavelength,
-                                             perturbations='known')
-            Phi = khatri_rao(A.conj(), A)
-            if not self._noise_known:
-                Phi = np.hstack((Phi, vec(np.eye(self._array.size))))
-            Phi = np.vstack((Phi.real, Phi.imag))
-            # Cache when possible.
-            if alt_grid is None and self._enable_caching:
-                self._atom_matrix = Phi
-            return Phi
-        else:
-            return self._atom_matrix
+    def _compute_atom_matrix(self, grid):
+        A = self._array.steering_matrix(
+            grid.source_placement, self._wavelength,
+            perturbations='known'
+        )
+        Phi = khatri_rao(A.conj(), A)
+        if not self._noise_known:
+            Phi = np.hstack((Phi, vec(np.eye(self._array.size))))
+        Phi = np.vstack((Phi.real, Phi.imag))
+        return Phi
 
-    def _call_solver(self, Phi, R, l, solver_options):
+    def _get_sparse_spectrum(self, Phi, R, l, solver_options):
         r = vec(R)
         r = np.vstack((r.real, r.imag))
         sol = self._problem.solve(Phi, r, l, **solver_options).flatten()
@@ -134,5 +124,36 @@ class SparseCovarianceMatching(SpectrumBasedEstimatorBase):
                 raise ValueError('sigma must be specified when noise variance is assumed known.')
             # Do not modify R in-place!
             R = R - np.eye(self._array.size) * sigma
-        return self._estimate(lambda Phi: self._call_solver(Phi, R, l, solver_options), k, **kwargs)
+        f_sp = lambda Phi: self._get_sparse_spectrum(Phi, R, l, solver_options)
+        return self._estimate(f_sp, k, **kwargs)
 
+class GroupSparseEstimator(SpectrumBasedEstimatorBase):
+    """Group-sparsity based estimator.
+    
+    References:
+    [1] D. Malioutov, M. Cetin, and A. S. Willsky, "A sparse signal
+        reconstruction perspective for source localization with sensor
+        arrays," IEEE Transactions on Signal Processing, vol. 53, no. 8,
+        pp. 3010-3022, Aug. 2005.
+    """
+
+    def __init__(self, array, wavelength, search_grid, n_snapshots, **kwargs):
+        super().__init__(array, wavelength, search_grid, **kwargs)
+        self._n_snapshots = n_snapshots
+        self._problem = L21RegularizedLeastSquaresProblem(
+            array.size, search_grid.size, n_snapshots, True
+        )
+    
+    def _get_sparse_spectrum(self, A, Y, l, solver_options):
+        X = self._problem.solve(A, Y, l, **solver_options)
+        return np.linalg.norm(X, ord=2, axis=1)
+
+    def estimate(self, Y, k, l, solver_options={}, **kwargs):
+        if 'refine_estimates' in kwargs:
+            raise ValueError('Grid refinement is not supported.')
+        if Y.shape[0] != self._array.size:
+            raise ValueError('The number of rows of Y must be equal to the array size.')
+        if Y.shape[1] != self._n_snapshots:
+            raise ValueError('The number of columns of Y must be equal to the number of snapshots.')
+        f_sp = lambda A: self._get_sparse_spectrum(A, Y, l, solver_options)
+        return self._estimate(f_sp, k, **kwargs)

doatools/optim/l1lsq.py
@@ -52,7 +52,7 @@ class L1RegularizedLeastSquaresProblem:
         """
         if not cvx_available:
             raise RuntimeError('Cannot initialize when cvxpy is not available.')
-        # Initialize parameters and variable.
+        # Initialize parameters and variables
         A = cvx.Parameter((m, k))
         b = cvx.Parameter((m, 1))
         l = cvx.Parameter(nonneg=True)
@@ -98,3 +98,60 @@ class L1RegularizedLeastSquaresProblem:
             warnings.warn('Optimal solution cannot be obtained.')
             return np.zeros((self._x.size,))
         return self._x.value
+
+class L21RegularizedLeastSquaresProblem:
+    r"""l-21 norm regularized least squares problem.
+    
+    The l-21 norm of a matrix variable X is given by
+        \|X\|_{2,1} = \sum_{i=1}^M \|X_i\|_2,
+    where X_i is the i-th row of X.
+
+    The l-21 norm regularized least squares problem is given by
+        min_X \frac{1}{2} \|AX - B\|_F^2 + l \|X\|_{2,1},
+    where A is M x K, X is K x L, B is M x L, and l is the regularization
+    parameter. Usually A is the dictionary matrix, X is the sparse signal to be
+    reconstructed, and B is the observation matrix where each column of B
+    represents a single observation.
+
+    Args:
+        m (int): Number of rows of the dictionary matrix A.
+        k (int): Number of rows of X (or the number of columns of the dictionary
+            matrix, A).
+        n (int): Number of observations (or the number of columns of the
+            observation matrix, B)
+    """
+
+    def __init__(self, m, k, n, complex=False):
+        if not cvx_available:
+            raise RuntimeError('Cannot initialize when cvxpy is not available.')
+        # Initialize parameters and variables
+        A = cvx.Parameter((m, k), complex=complex)
+        B = cvx.Parameter((m, n), complex=complex)
+        l = cvx.Parameter(nonneg=True)
+        X = cvx.Variable((k, n), complex=complex)
+        # Create the problem
+        obj_func = 0.5 * cvx.norm(cvx.matmul(A, X) - B, 'fro')**2 + \
+                   l * cvx.sum(cvx.norm(X.T, 2, axis=0))
+        self._problem = cvx.Problem(cvx.Minimize(obj_func))
+        self._A = A
+        self._B = B
+        self._l = l
+        self._X = X
+
+    def solve(self, A, B, l, **kwargs):
+        """Solves the problem with the specified parameters.
+
+        Args:
+            A: Dictionary matrix.
+            B: Observation matrix.
+            l: Regularization parameter.
+            **kwargs: Other keyword arguments to be passed to the solver.
+        """
+        self._A.value = A
+        self._B.value = B
+        self._l.value = l
+        self._problem.solve(**kwargs)
+        if self._problem.status != 'optimal':
+            warnings.warn('Optimal solution cannot be obtained.')
+            return np.zeros(self._X.shape)
+        return self._X.value